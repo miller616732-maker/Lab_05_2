f6b40a33d86e0731a4debc57f431026a53a2dc27
616732 <miller.616732@swl.k12.oh.us>
2025-09-17 08:55:54 -0400

    9/17/25 8:55

---
+++ src/Lab_05.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Lab_05 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int shoppingPrice = 0;
+        System.out.println("Please enter your shopping price");
+        if(scan.hasNextInt()) {
+            shoppingPrice = scan.nextInt();
+        }
+        }
+    }
