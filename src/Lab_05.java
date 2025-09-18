cb8fd30832256ce7e92e6e0a932d86d29092afd3
616732 <miller.616732@swl.k12.oh.us>
2025-09-18 08:25:41 -0400

    9/18/25 8:25

--- src/Lab_05.java
+++ src/Lab_05.java
@@ -2,10 +2,23 @@ import java.util.Scanner;
 public class Lab_05 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int shoppingPrice = 0;
+        double shoppingPrice = 0;
+        double taxRate = 0.02;
+        double shoppingTax;
+        double totalPrice;
         System.out.println("Please enter your shopping price");
-        if(scan.hasNextInt()) {
-            shoppingPrice = scan.nextInt();
+        if (scan.hasNextDouble()) {
+            shoppingPrice = scan.nextDouble();
+        } else {
+            System.out.println("You entered an invalid price");
+            System.exit(0);
+        }
+        if (shoppingPrice >= 100) {
+            System.out.println("Your total cost is " + shoppingPrice);
+        } else {
+            shoppingTax = shoppingPrice * taxRate;
+            totalPrice = shoppingPrice + shoppingTax;
+            System.out.println("Your total cost is " + totalPrice);
         }
         }
     }
